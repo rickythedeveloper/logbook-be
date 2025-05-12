a5cea0a1f0b36913fb8f85cc822125f635dc107f
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2025-05-12 21:30:24 +0100

    add log book entry type

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -1,3 +1,5 @@
+mod db;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::postgres::PgSeverity::Log;
@@ -15,27 +17,11 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
-#[derive(sqlx::FromRow)]
-struct LogbookEntry {
-    id: Uuid
-}
-
-async fn test_sql() -> Result<(), sqlx::Error> {
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect("postgres://postgres:postgres@localhost:5432/postgres").await?;
-
-    let row = sqlx::query_as::<_, LogbookEntry>("select * from logbook_entry").fetch_one(&pool).await?;
-
-    println!("{}", row.id);
-
-    Ok(())
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    test_sql().await.unwrap();
+    db::get_one_logbook_entry().await.unwrap();
 
     HttpServer::new(|| {
         App::new()
