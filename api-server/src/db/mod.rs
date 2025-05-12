c26a58780e51ac3ab05f51a1dde0f4ea50b59205
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2025-05-12 21:47:41 +0100

    error handling

--- api-server/src/db/mod.rs
+++ api-server/src/db/mod.rs
@@ -1,3 +1,4 @@
+use std::any::Any;
 use sqlx::postgres::PgPoolOptions;
 
 mod tables;
@@ -6,9 +7,16 @@ mod pool_utils;
 pub async fn get_one_logbook_entry() -> Result<(), sqlx::Error> {
     let pool = pool_utils::get_connection_pool().await;
 
-    let row = sqlx::query_as::<_, tables::logbook_entry::LogbookEntry>("select * from logbook_entry").fetch_one(&pool).await?;
+    let row = sqlx::query_as::<_, tables::logbook_entry::LogbookEntry>("select * from logbook_entry_2").fetch_one(&pool).await;
 
-    println!("{:?}", row);
+    match row {
+        Ok(row) => println!("{:?}", row),
+        Err(e) => match e {
+            sqlx::Error::RowNotFound => println!("No row found"),
+            sqlx::Error::Database(db_err) => println!("Database error: {:?}", db_err),
+            _ => println!("Error: {:?}", e),
+        }
+    }
 
     Ok(())
 }
