882635ace12c1fbd1af07faca3fdb27791aa89ca
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2025-05-12 22:15:29 +0100

    put server in modules

--- api-server/src/db/mod.rs
+++ api-server/src/db/mod.rs
@@ -8,7 +8,7 @@ pub async fn get_one_logbook_entry() -> Result<(), sqlx::Error> {
     let pool = pool_utils::get_connection_pool().await;
 
     let row =
-        sqlx::query_as::<_, tables::logbook_entry::LogbookEntry>("select * from logbook_entry_2")
+        sqlx::query_as::<_, tables::logbook_entry::LogbookEntry>("select * from logbook_entry")
             .fetch_one(&pool)
             .await;
 
--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -1,32 +1,11 @@
 mod db;
-
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+mod server;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     db::get_one_logbook_entry().await.unwrap();
 
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    server::start_server().await;
+
+    Ok(())
 }
